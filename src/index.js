8890c547c9ef06dc74f7f3aae940ea10e172a78f
avil13 <13.avil@gmail.com>
2017-07-16 15:38:24 +0300

    1.0.7

--- src/index.js
+++ src/index.js
@@ -1,33 +1,20 @@
 import swal from 'sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
 
+
 const VueSweetalert2 = function() {};
 
-VueSweetalert2.install = function(Vue, options) {
+VueSweetalert2.install = function(Vue) {
     // 1. добавление глобального метода или свойства
-    Vue.swal = function(...args) {
-        swal.apply(this, args);
-    };
-    //     // 2. добавление глобального объекта
-    // Vue.directive('my-directive', {
-    //         bind(el, binding, vnode, oldVnode) {
-    //                 // некоторая логика ...
-    //             }
-    //             ...
-    //     });
-    //
-    // // 3. добавление опций компонентов
-    // Vue.mixin({
-    //         created: function() {
-    //                 // некоторая логика ...
-    //             }
-    //             ...
-    //     });
+    Vue.swal = swal;
 
     // 4. добавление метода экземпляра
-    Vue.prototype.$swal = function(...args) {
-        swal.apply(this, args);
-    };
+    Object.defineProperty(Vue.prototype, '$swal', {
+        get: function get() {
+            return swal
+        }
+    });
 };
+
 
 export default VueSweetalert2;
