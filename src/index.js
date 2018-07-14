23c09927110939ce3a6d17cbcfc0e8115707a32d
avil13 <13.avil@gmail.com>
2018-07-14 11:21:31 +0300

    ssr

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import swal from 'sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
 
+function isBrowser() {
+    return typeof window !== 'undefined';
+}
+
 var VueSweetalert2 = function() {};
 
 VueSweetalert2.install = function(Vue, options = { toast: false }) {
     // 1. adding a global method or property
-    Vue.swal = swal;
+    const _swal = isBrowser() ? swal : function() {};
+
+    Vue.swal = _swal;
 
     // 4. add the instance method
     Object.defineProperty(Vue.prototype, '$swal', {
         get: function get() {
-            return swal;
+            return _swal
         }
     });
 };
