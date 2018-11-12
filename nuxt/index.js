64a006ab1045f1e0372b04befee8f56c2e9ad277
avil13 <13.avil@gmail.com>
2018-11-12 23:27:10 +0300

    fix nuxt

--- nuxt/index.js
+++ nuxt/index.js
@@ -1,12 +1,15 @@
-const { resolve } = require('path')
+const {
+    resolve
+} = require('path');
 
-module.exports = function nuxtVueSweetalert2 (moduleOptions) {
-  // Register plugin
-  this.addPlugin({
-    src: resolve(__dirname, 'plugin.js'),
-    fileName: 'vue-sweetalert2.js',
-    moduleOptions
-  })
+module.exports = function nuxtVueSweetalert2(moduleOptions) {
+    // Register plugin
+    this.addPlugin({
+        src: resolve(__dirname, 'plugin.js'),
+        fileName: 'vue-sweetalert2.js',
+        moduleOptions,
+        ssr: false
+    });
 }
 
-module.exports.meta = require('../package.json')
+module.exports.meta = require('../package.json');
