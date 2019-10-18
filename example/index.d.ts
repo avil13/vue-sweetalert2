a83f0c0ce05cce1aa549aca3d2da5bbcc12c56dd
Алексей Пивкин <apivkin@corp.finam.ru>
2019-10-18 16:47:32 +0300

    fix travis

--- example/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '*.vue' {
-    import Vue, {VueConstructor} from 'vue';
-    const _default: VueConstructor<Vue>;
-    // const _default: Vue
-    export default _default;
-}
--- example/src/main.ts
+++ example/src/main.ts
@@ -4,6 +4,8 @@ import App from './App.vue';
 
 import VueSweetalert2 from './vue-sweetalert2';
 
+import 'sweetalert2/dist/sweetalert2.min.css';
+
 Vue.use(VueSweetalert2);
 
 // tslint:disable-next-line: no-unused-expression
