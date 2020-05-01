a7b0867e111224359308283fbaabf14b0f82de4e
Batbayar Sukhbaatar <batbayar.sukhbaatar@grabtaxi.com>
2020-05-01 16:55:54 +0800

    nuxt: inject to context

--- nuxt/plugin.js
+++ nuxt/plugin.js
@@ -2,3 +2,6 @@ import Vue from 'vue';
 import VueSweetalert2 from 'vue-sweetalert2';
 
 Vue.use(VueSweetalert2, <%= JSON.stringify(options, null, 2) %>);
+export default ({}, inject) => {
+  inject('swal', Vue.swal)
+}
