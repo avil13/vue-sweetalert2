90d691b0182a953d87a5d2159e5bc355e88a6f61
avil13 <13.avil@gmail.com>
2020-06-29 17:49:00 +0300

    Revert "Disable force css including"
    This reverts commit a2f172880338a002e289f7a9b9266932166948ad.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 
-import Swal, {SweetAlertOptions} from 'sweetalert2/dist/sweetalert2.js';
+import Swal, {SweetAlertOptions} from 'sweetalert2';
 import swalLocalInstance from 'sweetalert2/dist/sweetalert2.js';
 
 type VueSwalInstance = typeof Swal.fire;
