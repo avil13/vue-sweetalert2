3e4b72cb46b798cafb6dbc0e40e2658284bdd3f3
Sébastien LeBlanc <mrsebastienleblanc@icloud.com>
2021-04-14 13:33:41 -0400

    Add Nuxt top-level option (#133)
    * [FEATURE] Add Nuxt top-level option
    
    Co-authored-by: Sébastien LeBlanc <seb110896@gmail.com>

--- packages/vue-sweetalert2/nuxt/index.js
+++ packages/vue-sweetalert2/nuxt/index.js
@@ -3,11 +3,13 @@ const {
 } = require('path');
 
 module.exports = function nuxtVueSweetalert2(moduleOptions) {
+    const options = Object.assign({}, this.options.sweetalert, moduleOptions)
+
     // Register plugin
     this.addPlugin({
         src: resolve(__dirname, 'plugin.js'),
         fileName: 'vue-sweetalert2.js',
-        options: moduleOptions,
+        options,
         ssr: false
     });
 }
