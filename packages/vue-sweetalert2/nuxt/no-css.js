307d1f5eeb7bd85efe4f79431005208cd97a1262
Sébastien LeBlanc <seb110896@gmail.com>
2021-04-14 13:29:17 -0400

    [FIX] Add Nuxt top-level option for no-css.js

--- packages/vue-sweetalert2/nuxt/no-css.js
+++ packages/vue-sweetalert2/nuxt/no-css.js
@@ -1,16 +1,16 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const {
-    resolve
-} = require('path');
+const {resolve} = require('path');
 
 module.exports = function nuxtVueSweetalert2(moduleOptions) {
-    // Register plugin
-    this.addPlugin({
-        src: resolve(__dirname, 'plugin.no-css.js'),
-        fileName: 'vue-sweetalert2.js',
-        options: moduleOptions,
-        ssr: false
-    });
-}
+  const options = Object.assign({}, this.options.sweetalert, moduleOptions);
+
+  // Register plugin
+  this.addPlugin({
+    src: resolve(__dirname, 'plugin.no-css.js'),
+    fileName: 'vue-sweetalert2.js',
+    options,
+    ssr: false,
+  });
+};
 
 module.exports.meta = require('../package.json');
