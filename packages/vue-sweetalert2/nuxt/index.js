9d44f1eb434054949d1bb67da831dab67906bb99
avil13 <13.avil@gmail.com>
2021-04-14 20:56:32 +0300

    chore: does package updates and minor fixes

--- packages/vue-sweetalert2/nuxt/index.js
+++ packages/vue-sweetalert2/nuxt/index.js
@@ -1,17 +1,16 @@
-const {
-    resolve
-} = require('path');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const {resolve} = require('path');
 
 module.exports = function nuxtVueSweetalert2(moduleOptions) {
-    const options = Object.assign({}, this.options.sweetalert, moduleOptions)
+  const options = Object.assign({}, this.options.sweetalert, moduleOptions);
 
-    // Register plugin
-    this.addPlugin({
-        src: resolve(__dirname, 'plugin.js'),
-        fileName: 'vue-sweetalert2.js',
-        options,
-        ssr: false
-    });
-}
+  // Register plugin
+  this.addPlugin({
+    src: resolve(__dirname, 'plugin.js'),
+    fileName: 'vue-sweetalert2.js',
+    options,
+    ssr: false,
+  });
+};
 
 module.exports.meta = require('../package.json');
